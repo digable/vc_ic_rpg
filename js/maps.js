89f17a2366209e88b55c4bcafec51222f0310bce
Quincy Fuller <quincy.fuller@gmail.com>
2026-02-15 11:29:51 -0600

    fixed a food vendor to access

--- js/maps.js
+++ js/maps.js
@@ -60,7 +60,7 @@ export const maps = {
       { x: 80, y: 48, name: 'Chef', type: 'food_cart', dialogue: ['I cook great ramen!', 'Try the Chef Special.'] },
       { x: 64, y: 112, name: 'Student', hasQuest: 'study_buddy', dialogue: ['I have so much homework...', 'Good luck out there!'] },
       { x: 224, y: 112, name: 'Bus Stop', type: 'cambus', isSign: true, dialogue: ['Cambus Stop'] },
-      { x: 192, y: 144, name: 'Food Cart Vendor', type: 'food_cart', dialogue: ['Fresh snacks!', 'Fuel up!'] }
+      { x: 192, y: 80, name: 'Food Cart Vendor', type: 'food_cart', dialogue: ['Fresh snacks!', 'Fuel up!'] }
     ],
     name: 'The Pentacrest',
     grassWalkable: true,
